4cb5b6ee958a9a01ab73e65ba2a4ccd3f52c696b
Benjamin <b.helmersson@gmail.com>
2016-10-17 21:11:28 +0200

    finish harmony memory

--- harmonysearch.py
+++ harmonysearch.py
@@ -9,15 +9,18 @@ from sudoku_classes import Solution, Grid
 
 class HarmonyMemory(object):
     
-    def __init__(self, problem, memory_size):
+    def __init__(self, problem, memory_size=50, HCR=.7, PAR=.1):
         self.problem = problem
         self.memory_size = memory_size
+        self.HCR = HCR
+        self.PAR = PAR
+        
         self.memory = [self.rand_solu(problem) for x in range(memory_size)]
         self.memory = [(s, s.evaluate()) for s in self.memory]
         self.update()
     
     def __repr__(self):
-        return str(self.memory)
+        return str(self.memory + [sum([x[1] for x in self.memory])])
         
     def update(self):
         self.memory.sort(key=lambda x: x[1])
@@ -29,57 +32,39 @@ class HarmonyMemory(object):
     def memory_consideration(self, square):
         return _r_.choice([h[0].numbers[square] for h in self.memory])
         
+    def insert_to_memory(self, new_harmony):
+        score = new_harmony.evaluate()
+        if score < self.memory[-1][1]:
+            self.memory.append((new_harmony, score))
+
+    def new_harmony(self):
+        square_list = []
+        for square in range(81):
+            b = self.get_boundaries(square)
+            if len(b) == 1: s = b[0]
+            elif _r_.random() > self.HCR: s = self.random_selection(b)
+            elif _r_.random() > self.PAR: s = self.memory_consideration(square)
+            else: s = self.pitch_adjustment(self.memory_consideration(square), b)
+            square_list.append(s)
+        return Solution(square_list)
+
+    def random_selection(self, boundaries):
+        return _r_.choice(boundaries)
     
+    def get_boundaries(self, square):
+        return self.problem[square]
 
-
-def random_selection(boundaries):
-    return _r_.choice(boundaries)
-
-
-def pitch_adjustment(square, boundaries):
-    
-    
-
-
-
-
-
-def insert_to_memory(harmony, hm):
-    score = harmony.evaluate()
-    if score < hm[-1][1]:
-        hm.insert()
-        hm.sort(key=lambda x: x[1])
-        hm  = hm[:-1]
-    else:
-        return None
-
-
-
-def improvise(HM, HCR, problem, PAR):
-    new_harmony = [{}, 0] # Empty non-rated harmony.
-    for square in problem: # For (x, y) on Sudoku board.
-        if _r_.random() <= HCR: # Considering HM.
-            n = _r_.randint(0, len(HM)-1) # Decides number for assignment.
-            new_harmony[0][key] = HM[n][0][key] # Assignment.
-        else: # Not considering HM.
-            if _r_.random() <= PAR: # Considering PA.
-                i = problem[0][key][1].index(HM[0][0][key])-1 # Index find.
-                new_harmony[0][key] = problem[0][key][1][i] # Assignment.
-            else: # Not considering PA = random.
-                new_harmony[0][key] = _r_.choice(problem[0][key][1])
-    evaluate_harmony(new_harmony) # Evaluate harmony score.
-    return new_harmony
-    
-
-
-
-def HM_add(HM, harmony):
-    for h in HM:
-        if harmony[1] > h[1]:
-            HM.insert(HM.index(h), harmony)
-            HM.pop(len(HM)-1)
-            break
-            
+    def pitch_adjustment(self, value, boundaries):
+        direction = _r_.choice([1, -1])
+        index = boundaries.index(value)
+        try: adjusted = boundaries[index+direction]
+        except IndexError: adjusted = boundaries[0]
+        return adjusted
+        
+    def improvise(self, times=1):
+        for x in range(times):
+            self.insert_to_memory(self.new_harmony())
+            self.update()
 
 
 def load_problem(problem_name="veryeasy1", heuristic=True):
